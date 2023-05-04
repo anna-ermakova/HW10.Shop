bc714ef605ae1a880497ee59528e0540a7e5a9ba
anna-ermakova <korovanna8@mail.ru>
2023-05-04 18:10:49 +0300

    Initial commit

---
+++ src/test/java/sky/pro/hw10/shop/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.hw10.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
